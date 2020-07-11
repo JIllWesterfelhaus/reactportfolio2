db5f7eceb2af39445ca5233f6704da754ca5fe0d
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-07-11 18:35:27 -0500

    updated names of png files

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import AboutMe from "./components/pages/AboutMe";
 import Portfolio from "./components/pages/Portfolio";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import './index.css';
+import  "./index.css";
 
 function App() {
   return (

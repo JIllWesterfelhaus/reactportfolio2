7a56a88512cff3f785e755abbac043770ea0ad73
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-08-19 09:53:28 -0500

    Updated About Me and Home pages.

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -10,9 +10,9 @@ function Home() {
         <img className="rounded-circle"style={{width: "35%"}} src={Headshot} alt="Jill Westerfelhaus" />
         </div>
         
-        <h5 className="text-center">I am a recent graduate of Northwestern University's Coding Bootcamp, where I studied Full Stack Web Development.</h5>  
+        <h5 className="text-center">I am a recent graduate of Northwestern University's Full Stack Web Development Coding Bootcamp.</h5>  
 
-      <h5 className="text-center">My skills include HTML, CSS, Bootstrap, Responsive Design, Javascript, JQuery, Node, Express, mySQL, Mongo, PWA's and React. I have a background in photography and I love working with Photoshop.</h5>
+      <h5 className="text-center">My skills include HTML, CSS, Bootstrap, Responsive Design, Javascript, JQuery, Node, Express, mySQL, Mongo, PWA's, React and Photoshop.</h5>
 
       <h5 className="text-center">I am currently an intern at <a href="https://www.queensweb.org/" target="_blank">Queen's Webs.</a></h5>
 

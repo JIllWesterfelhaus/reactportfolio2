552138d9b2609c487fd609b64b8a2bf1126b3dd7
JillWesterfelhaus <jillwesthaus@gmail.com>
2020-09-02 10:11:46 -0500

    updated Home and About Me

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -14,16 +14,15 @@ function Home() {
 
       <h5 className="text-center">My skills include HTML, CSS, Bootstrap, Responsive Design, Javascript, JQuery, Node, Express, mySQL, Mongo, PWA's, React and Photoshop.</h5>
 
-      <h5 className="text-center">I am currently an intern at <a href="https://www.queensweb.org/" target="_blank">Queen's Webs.</a></h5>
+      <h5 className="text-center">I am currently a software engineering intern at <a href="https://www.queensweb.org/" target="_blank">Queen's Web.</a>  Queen’s Web empowers women worldwide by providing professional opportunities for ongoing efforts to diversify the technology industry.</h5>
 
       <h5 className="text-center">Please take a look around and see what I learned!</h5>
 
-      <br></br>
       <h5 className="text-center">Email:  jillwesthaus@gmail.com</h5>
       <h5 className="text-center">Phone:  773-592-7923</h5>
       <h5 className="text-center"><a href="https://www.linkedin.com/in/jill-westerfelhaus-a821761a/" target="_blank">LinkedIn</a></h5>
       <h5 className="text-center"><a href="https://github.com/JIllWesterfelhaus" target="_blank">GitHub</a></h5>
-      <h5 className="text-center"><a href="https://drive.google.com/file/d/1uI29ld_OinWKOvuB_AKhaZKmx78qHQEb/view?usp=sharing" target="_blank">Resume</a></h5>
+      <h5 className="text-center"><a href="https://drive.google.com/file/d/1FVScz6kf1SYsEtsvBaCVVTNJ3bh4xsep/view?usp=sharing" target="_blank">Resume</a></h5>
 
     </div>
 
